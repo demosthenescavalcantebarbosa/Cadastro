7d5ab606a06dd7234244959ad9c1629d30727713
demosthenescavalcantebarbosa <demo1230987654@gmail.com>
2026-02-23 07:04:45 -0300

    first commit

--- src/main/java/dev/java10x/Cadastro/Cadastro.java
+++ src/main/java/dev/java10x/Cadastro/Cadastro.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CadastroApplication {
+public class Cadastro {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CadastroApplication.class, args);
+		SpringApplication.run(Cadastro.class, args);
 	}
 
 }
--- src/main/java/dev/java10x/Cadastro/NinjaModelo.java
+++ src/main/java/dev/java10x/Cadastro/NinjaModelo.java
@@ -5,7 +5,7 @@ import jakarta.persistence.*;
 // @Entity transforma uma classe em uma entidade do BD
 @Entity
 @Table(name = "ninja")
-public class Model {
+public class NinjaModelo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -13,9 +13,9 @@ public class Model {
     private String email;
     private int idade;
 
-    public Model(){}
+    public NinjaModelo(){}
 
-    public Model(String nome, String email, int idade) {
+    public NinjaModelo(String nome, String email, int idade) {
         this.nome = nome;
         this.email = email;
         this.idade = idade;
