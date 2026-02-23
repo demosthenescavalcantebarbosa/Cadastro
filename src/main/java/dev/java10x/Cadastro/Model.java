5c8c1eecd213f0d0edb24ed8c6e737448e86fd0f
demosthenescavalcantebarbosa <demo1230987654@gmail.com>
2026-02-23 06:27:57 -0300

    first commit

---
+++ src/main/java/dev/java10x/Cadastro/Model.java
@@ -0,0 +1,47 @@
+package dev.java10x.Cadastro;
+
+import jakarta.persistence.*;
+
+// @Entity transforma uma classe em uma entidade do BD
+@Entity
+@Table(name = "ninja")
+public class Model {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+
+    public Model(){}
+
+    public Model(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
